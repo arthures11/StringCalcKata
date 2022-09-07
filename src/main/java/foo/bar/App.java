b870adca87782e6ba8b8768bcd369dd6edb0599f
456 <58474213+arthures11@users.noreply.github.com>
2022-09-07 13:03:26 +0200

    fix ź/z

--- src/main/java/foo/bar/App.java
+++ src/main/java/foo/bar/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         StringCalc calc = new StringCalc();
-        System.out.println("Wprowadź string w celu dodania:\n");
+        System.out.println("Wprowadz string w celu dodania:\n");
         String numery = input.nextLine();
         int wynik = calc.add(numery);
         System.out.println("\nWynik dodawania to: "+wynik);
